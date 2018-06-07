5f95826ce624bba2c2873b7fa472a351c33de15e
Maurizio Pietrantuono <maurizio.pietrantuon@Mac2210.local>
2018-06-07 20:14:34 +0100

    Load image

---
+++ app/src/main/java/com/pppp/s/Extensions.kt
@@ -0,0 +1,10 @@
+package com.pppp.s
+
+import android.widget.ImageView
+import com.squareup.picasso.Picasso
+
+fun ImageView.loadImage(url: String?) {
+    Picasso.get()
+        .load(url)
+        .into(this)
+}
--- app/src/main/java/com/pppp/s/main/view/custom/MovieHolder.kt
+++ app/src/main/java/com/pppp/s/main/view/custom/MovieHolder.kt
@@ -2,10 +2,11 @@ package com.pppp.s.main.view.custom
 
 import android.support.v7.widget.RecyclerView
 import android.view.View
+import com.pppp.s.loadImage
 import com.pppp.s.main.model.pokos.Movie
+import kotlinx.android.synthetic.main.movie_item.view.*
 
-class MovieHolder(val view: View) : RecyclerView.ViewHolder(view) {
-    fun bindMovie(get: Movie?) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+class MovieHolder(val ew: View) : RecyclerView.ViewHolder(ew) {
+
+    fun bindMovie(movie: Movie?) = itemView.image.loadImage(movie?.poster)
 }
